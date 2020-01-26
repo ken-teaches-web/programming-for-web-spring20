e6509eab81b6989b92a4d43d7fedc1583d425027
Ken Korth <pixelfish22@gmail.com>
2020-01-26 16:38:53 -0600

    files produced for arrays video

---
+++ week2/arrays/arrays.js
@@ -0,0 +1,27 @@
+// variable but has multiple slots
+const myArr = ['bob', 'betty', 'sue'];
+const myOtherArray = new Array();
+myOtherArray.push('star wars'); // add items to end
+myOtherArray.push('raiders of the lost ark');
+// myArr.pop(); // removes last item
+// myArr.shift(); // removes first item
+myArr.unshift('james'); // adds item to the beginning of the array
+
+console.log(myArr);
+console.log(myOtherArray);
+console.log(myOtherArray[0]); // read single item
+console.log(myArr[1]);
+console.log(myArr.indexOf('betty'));
+const bettyIdx = myArr.indexOf('betty');
+console.log(bettyIdx); // if not found -1, if found real index number
+if (bettyIdx > -1) {
+    console.log(myArr[bettyIdx]);
+}
+myArr.splice(bettyIdx, 0, 'robert');
+console.log(myArr);
+console.log(myArr.length);
+// Math.random() for random 0 and 1
+// Math.round(n) rounds to nearest whole number
+// myArr.length - 1 to get the last index number of an array
+const randomNumber = Math.round(Math.random() * (myArr.length - 1));
+console.log(myArr[randomNumber]);
